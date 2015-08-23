78e6168bd088aae990d5f667905116928187f9e0
ptyxs <onyxxx@free.fr>
2015-08-23 15:34:59 +0200

    Variable x ajoutée.

--- TryingGit/src/fr/cassian/tryinggit/TryMain.java
+++ TryingGit/src/fr/cassian/tryinggit/TryMain.java
@@ -1,9 +1,11 @@
 package fr.cassian.tryinggit;
 
 public class TryMain {
-
+	static int x ;
 	public static void main(String[] args) {
 		System.out.println("début");
+		x += 23;
+		System.out.println("x = " + x);
 	}
 
 }
